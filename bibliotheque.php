49aa61bb0de8520843ac49715754840f59836ab3
hansoloukpona <oloukponah@gmail.com>
2025-03-18 03:07:40 +0100

    yumenews version 2020

---
+++ bibliotheque.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+	<head>
+ 		<meta charset="UTF-8" />
+ 		 <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="bootstrap/css/tuto.css" rel="stylesheet">
+ 		<link href="Yumenews.css" type="text/css" rel="stylesheet"/>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<header>
+				<div class="row">
+					<div class="col-sm-12">
+
+							<div ><img src="eyes.jpg" alt="yeux de hiboux" id="eyes"><div id="pagename">YumeNews</div><a href=""><sup id="sign">H</sup></a></div>
+
+					</div>
+				</div>
+			</header>
+
+			<div class="row">
+				<nav class="col-sm-12" class="yumemenu">
+					<h3>
+					<div class="row">
+
+						<a class="col-sm-2" href="index.php">Acceuil</a>
+						<a class="col-sm-2" href="Journal.php">Journal</a>
+						<a class="col-sm-offset-8 col-sm-2"  href="inscription.php" id="inscrire">S'inscrire</a>
+
+					</div>
+					</h3>
+				</nav>
+			</div>
+
+			<div class="row">
+				<section>
+				<aside class="col-sm-12"><h2 class="yumecontenu"> Bibliothèque YumeNews
+					</h2>
+
+				</aside>
+				<article class="col-sm-10 col-sm-push-2">
+					<p class="yumeindexarticle">
+
+					</p>
+				</article>
+
+
+			<nav class="col-sm-2 col-sm-pull-10" id="yumesearch">
+				</br>
+					<form method="post" action="">
+						<p class="recherche">
+						<label for="recherche"></label>Retrouver un ouvrage :<br/>
+						<input type="search" name="recherche" id="recherche" placeholder="Ex: le petit chaperon rouge"/>
+						</p>
+					</form>
+			</nav>
+		</section>
+		</div>
+
+			<footer class="row">
+						<div class="col-sm-4">
+
+							<form method="post" action="">
+								<p>
+								<label for="commentaire">Pour nous laisser un commentaire ou une impression :</label><br/>
+								<textarea name="commentaire" id="commentaire"></textarea>
+								<input type="submit" value="Envoyer"/>
+								</p>
+							</form>
+						</div>
+						<div class="col-sm-4">
+							<p>Pour suivre YumeNews :</p>
+							<p><a href="https:my.w.tt/LDyLaCkWK7">Wattpad</a></p>
+							<p><a href="https://www.facebook.com/107057034005845/posts/144885186889696/?app=fbl">Facebook</a></p>
+							<p><a href="https://discord.gg/ZpETCzB">Discord</a></p>
+							<p><a href="https://www.instagram.com/yumenewsofficiel">Instagram</a></p>
+
+						</div>
+						<div class="col-sm-4">
+							<p>Contacter un admin de YumeNews :</p>
+							<p>@kawouete</p>
+							<p>@As_Dyv</p>
+							<p>@Mmac33</p>
+						</div>
+					</div>
+				</footer>
+		</div>
+	</body>
---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+	<head>
+ 		<meta charset="UTF-8" />
+ 		 <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="bootstrap/css/tuto.css" rel="stylesheet">
+ 		<link href="Yumenews.css" type="text/css" rel="stylesheet"/>
+
+ 		<title>YumeNews</title>
+
+	</head>
+	<body background="yumebg.jpeg">
+
+		<div class="container-fluid">
+			<header>
+				<div class="row">
+					<div class="col-sm-12">
+
+							<div ><img src="yumelogo1.png" alt="yeux de hiboux" id="eyes"><div id="pagename">YumeNews</div><a href="mailto:oloukponah@gmail.com" title="le concepteur"><sup id="sign">H</sup></a></div>
+
+					</div>
+				</div>
+			</header>
+
+			<div class="row">
+				<nav class="yumemenu">
+					<h3>
+						<a class="col-sm-2" href="index.php">Acceuil</a>
+						<a class="col-sm-2" href="Journal.php" >Journal</a>
+						<a class="col-sm-offset-6 col-sm-2"  href="inscription.php" id="inscrire">S'inscrire</a>
+					</div>
+					</h3>
+				</nav>
+			</div>
+
+
+
+			<div class="row">
+				<section>
+				<aside class="col-sm-12"><h2 class="yumecontenu"> Bienvenue sur Yumenews
+					</h2>
+
+				</aside>
+				<article class="col-sm-10 col-sm-push-2">
+					<p class="yumecontenu">
+					YumeNews est une plateforme dédié à l'imformation et la culture génrale par la lecture. Notre site Web fait partie d'un vaste projet de valorisation de la lecture et de l'écriture qui s'étend sur plusieurs front dont le sien est la promotion de livre de tout genre et d'auteurs débutants et leur suivi par une communauté de personnes ayant une expérience certaine à faire valoir mais aussi désireux d'en acquérir...</br>
+					YumeNews propose de <a href="">concours</a> de lecture, d'écriture et de culture générale pour distraire utilement ses adhérants avec des récompenses intéressantes à la clé... Sa <a href="bibliotheque.html">bibliothèque</a> dispose d'une Variété interressante de livre inédit à lire sur <a href="">Wattpad</a> ou en version numérique téléchargeable. Par ailleurs une liste actualisée des ouvrages recommandés par les lecteurs adhérants est disponible sur nos <a href="">réseaux sociaux</a> du Yume...</br>
+					YumeNews est aussi la vitrine qui permet de suivre l'évolution de certains de écrivains en herbe qui participent au programme d'encadrement <a href="">YumeHelp</a>... Un <a href="">blog</a> libre et ouvert incluant des sujets hebdomadaire et la possibilité de proposer ses propres sujets de discussions est disponible sur la plate-forme pour les échanges publics,les débats, etc.</br>
+					Un espace <a href="">Membres Influents</a> est dédié aux adhérants les plus actifs, contributeurs et dynamiques de la communauté et nous tenons un <a href="Journal.html">Journal</a> ou sont consigné toutes les évolutions et modificatons passées, actuels et futurs liées à la plate-forme...
+					</p>
+				</article>
+
+				<nav class="col-sm-2 col-sm-pull-10" id="yumesearch">
+					</br>
+					<form method="post" action="">
+						<p class="recherche">
+						<label for="recherche">Retrouver un ouvrage :</label><br/>
+						<input type="search" name="recherche" id="recherche" placeholder="Ex: le petit chaperon rouge"/>
+						</p>
+					</form>
+				</nav>
+				</section>
+			</div>
+				<footer class="row">
+						<div class="col-sm-4">
+
+							<form method="post" action="">
+								<p>
+								<label for="commentaire">Pour nous laisser un commentaire ou une impression :</label><br/>
+								<textarea name="commentaire" id="commentaire"></textarea>
+								<input type="submit" value="Envoyer"/>
+								</p>
+							</form>
+						</div>
+						<div class="col-sm-4">
+							<p>Pour suivre YumeNews :</p>
+							<p><a href="https:my.w.tt/LDyLaCkWK7">Wattpad</a></p>
+							<p><a href="https://www.facebook.com/107057034005845/posts/144885186889696/?app=fbl">Facebook</a></p>
+							<p><a href="https://discord.gg/ZpETCzB">Discord</a></p>
+							<p><a href="https://www.instagram.com/yumenewsofficiel">Instagram</a></p>
+						</div>
+						<div class="col-sm-4">
+							<p>Contacter un admin de YumeNews :</p>
+							<p>@kawouete</p>
+							<p>@As_Dyv</p>
+							<p>@Mmac33</p>
+						</div>
+					</div>
+				</footer>
+		</div>
+	</body>
+</html>
